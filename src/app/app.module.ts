f0011f05ec3f97a8d9001d3a334b95ec393ec4c2
tencho12 <tenzincho74@gmail.com>
2019-05-28 01:21:46 +0600

    todo one

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+//import this to use http module
+import { HttpClientModule } from '@angular/common/http'
+//import this to use form
+import {FormsModule} from '@angular/forms'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { AddTodoComponent } from './components/add-todo/add-todo.component';
+import { AboutComponent } from './components/pages/about/about.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    HeaderComponent,
+    AddTodoComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule, //after the import, should be written here too
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
